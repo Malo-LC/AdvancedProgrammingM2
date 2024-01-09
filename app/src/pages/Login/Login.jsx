0b0b685e2eb4e4a1300bf1e20171dd896cbb0696
Enrique <73123851+F4l4X@users.noreply.github.com>
2024-01-10 00:03:34 +0100

    Login page (#2)
    * login page design
    
    * adding input logos
    
    * password visibility and css
    
    * login button style

---
+++ app/src/pages/Login/Login.jsx
@@ -0,0 +1,73 @@
+import { useForm } from "react-hook-form";
+import api from "../../utils/api";
+import { Link, useNavigate } from "react-router-dom";
+import "./login.css";
+
+//assets
+import logo_efrei from "../../assets/images/logo_efrei.png";
+import mail from "../../assets/images/icons/mail.png";
+import lock from "../../assets/images/icons/lock.png";
+import EyeOff from "../../assets/images/icons/EyeOff.png";
+
+function Login() {
+  const navigate = useNavigate();
+
+  const { register, handleSubmit, errors } = useForm({
+    mode: "onBlur",
+  });
+
+  const onSubmit = (data) => {
+    api
+      .post("auth/authenticate", data)
+      .then((res) => {
+        api.setToken(res.access_token);
+        navigate("/home");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  function togglePasswordVisibility(e) {
+    const input = e.target.parentElement.previousElementSibling;
+    const type = input.type === "password" ? "text" : "password";
+
+    input.type = type;
+  }
+
+  return (
+    <div className="h-screen">
+      <div className="login-page">
+        <div className="login-component">
+          <img src={logo_efrei} alt="efrei_logo" className="logo" />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="relative">
+              <img src={mail} alt="mail_icon" className="input-icon-left" />
+              <input placeholder="Email" className="input" type="email" {...register("email", { required: true })} />
+              {errors?.email && <span>Email is required</span>}
+            </div>
+            <div className="relative">
+              <img src={lock} alt="lock_icon" className="input-icon-left" />
+              <input placeholder="Password" className="input" type="password" {...register("password", { required: true })} />
+              {errors?.password && <span>Password is required</span>}
+              <button type="button" onClick={(e) => togglePasswordVisibility(e)}>
+                <img src={EyeOff} alt="" className="input-icon-right" />
+              </button>
+            </div>
+            <div className="button-container">
+              <input type="submit" value="Login"></input>
+              <div className="no-account">
+                <p>No account?</p>
+                <Link to="/register" className="register-link ">
+                  Register
+                </Link>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
