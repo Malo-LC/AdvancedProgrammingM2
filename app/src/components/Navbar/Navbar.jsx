8a9a329262bcf46594bcda86ea6f918400ed6650
Enrique <73123851+F4l4X@users.noreply.github.com>
2024-01-10 15:00:00 +0100

    npm i

---
+++ app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import logo_efrei from "../../assets/images/logo_efrei.png";
+
+export function Navbar() {
+  return (
+    <div className="navbar">
+      <img src={logo_efrei} className="logo" />
+    </div>
+  );
+}
