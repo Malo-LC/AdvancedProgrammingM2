bdd9699b0f4aa5921e870f8391ce3412569ed40d
Enrique <73123851+F4l4X@users.noreply.github.com>
2024-01-11 17:04:36 +0100

    navbar and mobile version

--- app/src/components/ProfileNav/ProfileNav.jsx
+++ app/src/components/ProfileNav/ProfileNav.jsx
@@ -4,17 +4,31 @@ import PropTypes from "prop-types";
 import { Notification } from "../BasicComponents/Notification/Notification";
 import { Avatar } from "@mui/material";
 
-export function ProfileNav({ profilePicture, firstname, lastname }) {
+export function ProfileNav({ profilePicture, firstname, lastname, isMobile }) {
   return (
     <div className="profile">
-      <Notification notificationNumber={4} />
-      <div className="flex flex-row space-x-1">
-        <p>{firstname}</p>
-        <p>{lastname}</p>
-      </div>
-      <button>
-        <Avatar alt={firstname} src={profilePicture} sx={{ width: 35, height: 35 }} />
-      </button>
+      {isMobile ? (
+        <>
+          <button>
+            <Avatar alt={firstname} src={profilePicture} sx={{ width: 35, height: 35 }} />
+          </button>
+        </>
+      ) : (
+        <>
+          <Notification notificationNumber={4} />
+          <div className="flex flex-row space-x-1">
+            <p>{firstname}</p>
+            <p>{lastname}</p>
+          </div>
+          <button
+            onClick={() => {
+              console.log(isMobile);
+            }}
+          >
+            <Avatar alt={firstname} src={profilePicture} sx={{ width: 35, height: 35 }} />
+          </button>
+        </>
+      )}
     </div>
   );
 }
@@ -23,4 +37,5 @@ ProfileNav.propTypes = {
   profilePicture: PropTypes.string.isRequired,
   firstname: PropTypes.string.isRequired,
   lastname: PropTypes.string.isRequired,
+  isMobile: PropTypes.bool.isRequired,
 };
