8d86b31481ea2ee0ea3e1ccd1531832f144b21f3
Enrique <73123851+F4l4X@users.noreply.github.com>
2024-01-15 22:11:18 +0100

    test

--- api/src/main/java/com/advancedprogramming/api/models/Filedb.java
+++ api/src/main/java/com/advancedprogramming/api/models/Filedb.java
@@ -4,25 +4,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UuidGenerator;
-
-import java.util.List;
 
 @Data
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Getter
-@Table(name = "filedb")
-public class Filedb {
+public class FileDB {
     @Id
     @UuidGenerator
     private String id;
@@ -33,10 +24,10 @@ public class Filedb {
     @Column(columnDefinition = "MEDIUMBLOB")
     private byte[] data;
 
-    @OneToMany(mappedBy = "filedb")
-    private List<Submit> submits;
+    public FileDB() {
+    }
 
-    public Filedb(String name, String type, byte[] data) {
+    public FileDB(String name, String type, byte[] data) {
         this.name = name;
         this.type = type;
         this.data = data;
