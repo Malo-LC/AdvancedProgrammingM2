2f39d53372f1dd8c371098e148ee8f357f548b64
Enrique <73123851+F4l4X@users.noreply.github.com>
2024-01-16 10:33:56 +0100

    navbarUserInfo retrieving

--- app/src/components/Navbar/Navbar.jsx
+++ app/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,22 @@
-import logo_efrei_white from "../../assets/images/logo_efrei_white.png";
-import { NavTab } from "../BasicComponents/NavTab/NavTab.jsx";
-import { ProfileNav } from "../ProfileNav/ProfileNav";
-import { useMediaQuery } from "react-responsive";
+import DescriptionIcon from "@mui/icons-material/Description";
+import HomeIcon from "@mui/icons-material/Home";
 import MenuIcon from "@mui/icons-material/Menu";
-import { useState } from "react";
-import Drawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
 import ListItemIcon from "@mui/material/ListItemIcon";
-import HomeIcon from "@mui/icons-material/Home";
-import DescriptionIcon from "@mui/icons-material/Description";
+import ListItemText from "@mui/material/ListItemText";
+import { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import { useNavigate } from "react-router-dom";
+import logo_efrei_white from "../../assets/images/logo_efrei_white.png";
+import { NavTab } from "../BasicComponents/NavTab/NavTab.jsx";
+import { ProfileNav } from "../ProfileNav/ProfileNav";
+
+import userService from "../../services/userService.js";
+import api from "../../utils/api.js";
 
 //style
 import "./navbar.css";
@@ -22,7 +25,31 @@ export default function Navbar() {
   const navigate = useNavigate();
   const isMobile = useMediaQuery({ maxWidth: 767 });
 
+  const [user, setUser] = useState(null);
   const [showNavbar, setShowNavbar] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    console.log(token);
+
+    const fetchUserInfo = async () => {
+      if (token) {
+        api.setToken(token);
+        try {
+          const userInfo = await userService.getUserInfo();
+          console.log(userInfo);
+          if (userInfo) {
+            setUser(userInfo);
+          }
+        } catch (error) {
+          console.log("Error fetching user info: ", error);
+        }
+      }
+    };
+
+    fetchUserInfo();
+  }, []);
 
   const toggleDrawer = (open) => (event) => {
     if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
@@ -66,23 +93,29 @@ export default function Navbar() {
           <div>
             <img src={logo_efrei_white} className="h-[30px]" />
           </div>
-          <div className="profile">
-            <ProfileNav isMobile={isMobile} firstname="James" lastname="Jacob" />
-          </div>
+          {user && (
+            <div className="profile">
+              <ProfileNav isMobile={isMobile} firstname={user.firstname} lastname={user.lastname} />
+            </div>
+          )}
         </>
       ) : (
         <>
-          <div>
+          <div className="flex-none">
             <img src={logo_efrei_white} className="h-[40px]" />
           </div>
-          <div className="test">
+          <div className="flex-grow">
             <nav className="nav-element-container">
               <NavTab name="Home" />
               <NavTab name="Documents" />
             </nav>
           </div>
-          <div className="profile">
-            <ProfileNav isMobile={isMobile} firstname="James" lastname="Jacob" />
+          <div className="flex-none">
+            {user && (
+              <div className="profile">
+                <ProfileNav isMobile={isMobile} firstname={user?.firstname} lastname={user?.lastname} />
+              </div>
+            )}
           </div>
         </>
       )}
