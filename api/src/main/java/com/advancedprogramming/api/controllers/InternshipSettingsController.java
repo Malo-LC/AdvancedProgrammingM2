4fcea60363ba659b6601d3b988d130fdd5136b22
Malo Le Corvec <malolc2001@gmail.com>
2024-01-22 17:40:15 +0100

    update the internship settings

--- api/src/main/java/com/advancedprogramming/api/controllers/InternshipSettingsController.java
+++ api/src/main/java/com/advancedprogramming/api/controllers/InternshipSettingsController.java
@@ -1,5 +1,6 @@
 package com.advancedprogramming.api.controllers;
 
+import com.advancedprogramming.api.controllers.beans.MessageResponse;
 import com.advancedprogramming.api.models.User;
 import com.advancedprogramming.api.models.bean.RoleEnum;
 import com.advancedprogramming.api.services.InternshipSettingsService;
@@ -12,6 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,5 +41,22 @@ public class InternshipSettingsController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
         return ResponseEntity.ok(internshipSettings);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<MessageResponse> updateInternshipSettings(
+        HttpServletRequest request,
+        @RequestBody InternshipSettings internshipSettings
+    ) {
+        User user = userService.getUserByFromRequest(request);
+        if (user == null || !user.getRole().equals(RoleEnum.ADMIN)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+
+        Boolean success = internshipSettingsService.updateInternshipSettings(internshipSettings);
+        if (Boolean.FALSE.equals(success)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessageResponse("Internship not found", false));
+        }
+        return ResponseEntity.ok(new MessageResponse("Internship updated", true));
     }
 }
--- api/src/main/java/com/advancedprogramming/api/services/bean/RequiredReport.java
+++ api/src/main/java/com/advancedprogramming/api/services/bean/RequiredReport.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 public record RequiredReport(
     Integer id,
-    String name,
+    String title,
     String description,
     LocalDate deadline
 ) {
