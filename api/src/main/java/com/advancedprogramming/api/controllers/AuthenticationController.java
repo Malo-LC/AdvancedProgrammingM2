b541b1743ba7a9fc2801dd26cd2c13005d6002a8
Malo Le Corvec <malolc2001@gmail.com>
2024-01-23 13:06:32 +0100

    document api for swagger

--- api/src/main/java/com/advancedprogramming/api/controllers/AuthenticationController.java
+++ api/src/main/java/com/advancedprogramming/api/controllers/AuthenticationController.java
@@ -15,6 +15,11 @@ import com.advancedprogramming.api.controllers.beans.AuthenticationResponse;
 import com.advancedprogramming.api.controllers.beans.RegisterRequest;
 import com.advancedprogramming.api.services.AuthenticationService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -23,6 +28,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin(origins = "*")
+@Tag(name = "User Authentication")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
@@ -30,6 +36,11 @@ public class AuthenticationController {
     private final LogoutService logoutService;
 
     @PostMapping("/register")
+    @Operation(summary = "Register a new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User registered successfully", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "Bad request")
+    })
     public ResponseEntity<AuthenticationResponse> register(
             @Valid @RequestBody RegisterRequest request) throws IOException {
         if (request.profilePicture() == null) {
@@ -38,12 +49,14 @@ public class AuthenticationController {
         return ResponseEntity.ok(authService.register(request));
     }
 
+    @Operation(summary = "Authenticate a user")
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(authService.authenticate(request));
     }
 
+    @Operation(summary = "Logout a user")
     @PostMapping("/logout")
     public ResponseEntity<Void> logout(
             HttpServletRequest request,
