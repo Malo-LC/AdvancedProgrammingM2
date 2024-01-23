1ac6008cb9b233d86cd0981e52e9cc184bdeca86
Enrique <73123851+F4l4X@users.noreply.github.com>
2024-01-23 18:34:20 +0100

    footer fix (#32)

--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -4,8 +4,8 @@ import { useMediaQuery } from "react-responsive";
 import api from "../../utils/api";
 
 //style
-import footer from "../../assets/images/footer.png";
-import homepage_img from "../../assets/images/homepage_img.png";
+import footer from "../../assets/images/footer.svg";
+import homepage_img from "../../assets/images/homepage_img.svg";
 import "./home.css";
 
 function Home() {
@@ -35,10 +35,10 @@ function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col items-center">
       <motion.div className={`homepage ${isMobile ? "homepage-mobile" : "homepage-desktop"}`}>
         <motion.div className="text-div" variants={textDivVariants} initial="hidden" animate="visible">
-          <motion.div className="text-[40px] sm:text-[50px] md:text-[70px] text-[#163767] font-bold" variants={textItemVariants}>
+          <motion.div className="text-[40px] sm:text-[50px] md:text-[70px] 2xl:text-[90px] text-[#163767] font-bold" variants={textItemVariants}>
             <h1>MYEFREI STAGE</h1>
           </motion.div>
           <motion.div className="text-[#6D7ED6] text-[25px] md:text-[35px] font-semibold" variants={textItemVariants}>
@@ -53,8 +53,8 @@ function Home() {
         </motion.div>
       </motion.div>
 
-      <div className="absolute bottom-0 w-full">
-        <img src={footer} />
+      <div className="absolute bottom-0 w-screen">
+        <img src={footer} className="w-full" />
       </div>
     </div>
   );
