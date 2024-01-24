5ce0ae80079a1fcf4d9867698c487dd4b5616397
Enrique <73123851+F4l4X@users.noreply.github.com>
2024-01-24 16:44:48 +0100

    responsive doc

--- app/src/components/DocElement/DocElement.jsx
+++ app/src/components/DocElement/DocElement.jsx
@@ -1,32 +1,88 @@
 import Status from "../BasicComponents/Status/Status";
 import ValidationBubble from "../BasicComponents/ValidationBubble/ValidationBubble";
 import ActionButton from "../BasicComponents/ActionButton/ActionButton";
+import { useMediaQuery } from "react-responsive";
+import React, { useState } from "react";
+import { motion, useAnimation } from "framer-motion";
 
 //style
 import "./docelement.css";
 // ${userRole === "STUDENT" ? "w-[850px]" : "w-full"}
 function DocElement({ name, deadline, student_name, validation_name, status, userRole }) {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  // const controls = useAnimation();
+  const [isExpanded, setIsExpanded] = useState(false);
+  const mobileElementHeight = userRole === "STUDENT" ? 130 : 90;
+
+  const handleClick = async () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div className="flex flex-row items-center space-x-2 ">
-      <div className={`docelement justify-between w-full `}>
-        <div className="elements bg-purple-100">{name}</div>
-        <div className="elements font-thin">{deadline}</div>
-        <div className="elements">{student_name}</div>
-        <div className="elements space-x-1">
-          <ValidationBubble validationStatus="validated" firstname="Stephane" lastname="Plaza" />
-          <ValidationBubble validationStatus="notValidated" firstname="Didier" lastname="Bourdon" />
-          <ValidationBubble validationStatus="notTreated" firstname="Didier" lastname="Bourdon" />
+    <>
+      {isMobile ? (
+        <motion.div className="docelement-mobile">
+          <button onClick={handleClick} className="flex flex-row justify-between w-full">
+            <div className="elements text-xl">{name}</div>
+            <div className="elements">
+              <Status status={status} />
+            </div>
+          </button>
+          <motion.div className="expanded-content" animate={{ opacity: isExpanded ? 1 : 0, height: isExpanded ? mobileElementHeight : 0 }}>
+            {isExpanded && (
+              <>
+                {student_name && (
+                  <div className="elements-hidden">
+                    <p className="mr-2">Nom étudiant :</p>
+                    <p className="font-thin">{student_name}</p>
+                  </div>
+                )}
+                <div className="elements-hidden">
+                  <p className="mr-2">Date de rendue :</p>
+                  <p className="font-thin">{deadline}</p>
+                </div>
+                <div className="elements-hidden space-x-1 mt-1">
+                  <p className="mr-2">Validations :</p>
+                  <div className="flex flex-row space-x-2">
+                    <ValidationBubble validationStatus="validated" firstname="Stephane" lastname="Plaza" />
+                    <ValidationBubble validationStatus="notValidated" firstname="Didier" lastname="Bourdon" />
+                    <ValidationBubble validationStatus="notTreated" firstname="Didier" lastname="Bourdon" />
+                  </div>
+                </div>
+                <div className="w-full flex justify-center mt-2">
+                  {userRole === "STUDENT" && (
+                    <>
+                      <ActionButton status={status} />
+                    </>
+                  )}
+                </div>
+              </>
+            )}
+          </motion.div>
+        </motion.div>
+      ) : (
+        <div className="flex flex-row items-center space-x-2 ">
+          <div className={`docelement justify-between w-full `}>
+            <div className="elements">{name}</div>
+            <div className="elements font-thin">{deadline}</div>
+            <div className="elements">{student_name}</div>
+            <div className="elements space-x-1">
+              <ValidationBubble validationStatus="validated" firstname="Stephane" lastname="Plaza" />
+              <ValidationBubble validationStatus="notValidated" firstname="Didier" lastname="Bourdon" />
+              <ValidationBubble validationStatus="notTreated" firstname="Didier" lastname="Bourdon" />
+            </div>
+            <div className="elements">
+              <Status status={status} />
+            </div>
+          </div>
+          {userRole === "STUDENT" && (
+            <>
+              <ActionButton status={status} />
+            </>
+          )}
         </div>
-        <div className="elements">
-          <Status status={status} />
-        </div>
-      </div>
-      {userRole === "STUDENT" && (
-        <>
-          <ActionButton status={status} />
-        </>
       )}
-    </div>
+    </>
   );
 }
 
--- app/src/pages/Documents/Documents.jsx
+++ app/src/pages/Documents/Documents.jsx
@@ -4,6 +4,7 @@ import Searchbar from "../../components/BasicComponents/SearchBar/SearchBar";
 import DocElement from "../../components/DocElement/DocElement";
 import userService from "../../services/userService";
 import api from "../../utils/api";
+import { useMediaQuery } from "react-responsive";
 
 //style
 import "./documents.css";
@@ -13,8 +14,9 @@ const docColumnNamesStudent = ["Nom du document", "Deadline", "Intitulé du stag
 
 function Documents() {
   const controls = useAnimation();
+  const isMobile = useMediaQuery({ maxWidth: 767 });
 
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
   const [userRole, setUserRole] = useState(null);
 
   let docColumn;
@@ -27,11 +29,11 @@ function Documents() {
       if (token) {
         api.setToken(token);
         try {
-          const userInfo = await userService.getUserInfo();
+          // const userInfo = await userService.getUserInfo();
           const userRole = await userService.getRole();
-          if (userInfo) {
-            setUser(userInfo);
-          }
+          // if (userInfo) {
+          //   setUser(userInfo);
+          // }
           if (userRole) {
             setUserRole(userRole);
           }
@@ -51,24 +53,31 @@ function Documents() {
   }
 
   return (
-    <div>
-      <motion.div className="page-header" initial={{ x: "-300%" }} animate={{ x: 0 }} transition={{ type: "spring", stiffness: 120, damping: 14 }}>
+    <div className={`documents ${isMobile ? "items-start" : "items-center justify-center"}`}>
+      <motion.div
+        className={`page-header ${isMobile ? "flex-col" : "flex-row"}`}
+        initial={{ x: "-300%" }}
+        animate={{ x: 0 }}
+        transition={{ type: "spring", stiffness: 120, damping: 14 }}
+      >
         <p className="document-title">Mes Documents</p>
         <div className="flex flex-row">
           <Searchbar />
         </div>
       </motion.div>
       <div className="table">
-        <div className="table-title">
-          {userRole &&
-            docColumn.map((item, index) => (
-              <div className="font-semibold w-[150px] text-center" key={index}>
-                {item}
-              </div>
-            ))}
-        </div>
-        <div className="doc-container">
-          <DocElement name="test" deadline="01/01/01" student_name="test" validation="test" status="delivered" userRole={userRole} />
+        {isMobile === false && (
+          <div className="table-title">
+            {userRole &&
+              docColumn.map((item, index) => (
+                <div className="font-semibold text-center" key={index}>
+                  {item}
+                </div>
+              ))}
+          </div>
+        )}
+        <div className={`doc-container ${isMobile ? "w-screen items-center px-10 h-[700px]" : "h-[550px]"}`}>
+          <DocElement name="Rapport de stage" deadline="02/08/24" student_name="Don Diego" validation="test" status="delivered" userRole={userRole} />
           <DocElement name="test" deadline="01/01/01" student_name="test" validation="test" status="not_delivered" userRole={userRole} />
           <DocElement name="test" deadline="01/01/01" student_name="test" validation="test" status="not_delivered" userRole={userRole} />
           <DocElement name="test" deadline="01/01/01" student_name="test" validation="test" status="not_delivered" userRole={userRole} />
