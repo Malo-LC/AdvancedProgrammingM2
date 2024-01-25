c2ebb1dfe76332689806da4c6a0c1a712e7a9a5b
Enrique <73123851+F4l4X@users.noreply.github.com>
2024-01-25 12:20:09 +0100

    conditional renderin status + type props added (#46)

--- app/src/components/BasicComponents/Status/Status.jsx
+++ app/src/components/BasicComponents/Status/Status.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { CheckCircle } from "react-feather";
 import { AlertTriangle } from "react-feather";
 
@@ -6,18 +5,41 @@ import { AlertTriangle } from "react-feather";
 import "./status.css";
 import PropTypes from "prop-types";
 
-function Status({ status }) {
-  return (
-    <div className={`status ${status === true ? "bg-[#5CBF92]" : "bg-[#DF3A3A]"}`}>
-      {status === false ? <CheckCircle className="text-white" /> : <AlertTriangle className="text-white" />}
+function Status({ status, type }) {
+  let icon, text, colorClass;
 
-      <p className="text-white font-extralight">{status === "delivered" ? "Remis" : "Non remis"}</p>
+  if (status === true && type === "document") {
+    icon = <CheckCircle className="text-white" />;
+    text = "Remis";
+    colorClass = "bg-[#5CBF92]";
+  } else if (status === false && type === "document") {
+    icon = <AlertTriangle className="text-white" />;
+    text = "Non remis";
+    colorClass = "bg-[#DF3A3A]";
+  } else if (status === false && type === "demande") {
+    icon = <AlertTriangle className="text-white" />;
+    text = "Refusé";
+    colorClass = "bg-[#DF3A3A]";
+  } else if (status === true && type === "demande") {
+    icon = <AlertTriangle className="text-white" />;
+    text = "Accepté";
+    colorClass = "bg-[#DF3A3A]";
+  } else if (status === null && type === "demande") {
+    text = "En attente";
+    colorClass = "bg-[#cccccc]";
+  }
+
+  return (
+    <div className={`status ${colorClass}`}>
+      {icon}
+      <p className={`${status === null ? "text-[#4A4545]" : "text-white"} font-extralight`}>{text}</p>
     </div>
   );
 }
 
 Status.propTypes = {
-  status: PropTypes.string.isRequired,
+  status: PropTypes.bool.isRequired,
+  type: PropTypes.string.isRequired,
 };
 
 export default Status;
