e8bde530e6aef200f4efe0d4e8a94831c6b149cc
Malo Le Corvec <malolc2001@gmail.com>
2024-01-25 09:51:51 +0100

    Get all submits for tutors + fix StudentInternship

--- api/src/main/java/com/advancedprogramming/api/models/StudentInternshipRepository.java
+++ api/src/main/java/com/advancedprogramming/api/models/StudentInternshipRepository.java
@@ -1,6 +1,7 @@
 package com.advancedprogramming.api.models;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,8 @@ import java.util.List;
 public interface StudentInternshipRepository extends JpaRepository<StudentInternship, Integer> {
 
     List<StudentInternship> findAllByUserId(Integer userId);
+
+    @Query("SELECT si FROM StudentInternship si WHERE si.tutorSchoolUser.id = ?1 or si.tutorCompanyUser.id = ?1")
+    List<StudentInternship> findAllByTutorId(Integer tutorId);
 
 }
