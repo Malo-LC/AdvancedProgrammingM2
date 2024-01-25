b83e3a18a2cc43a859e7e5ea2c53885e1a501900
Enrique <73123851+F4l4X@users.noreply.github.com>
2024-01-25 11:09:48 +0100

    getting docs from db

--- app/src/components/BasicComponents/Status/Status.jsx
+++ app/src/components/BasicComponents/Status/Status.jsx
@@ -8,8 +8,8 @@ import PropTypes from "prop-types";
 
 function Status({ status }) {
   return (
-    <div className={`status ${status === "delivered" ? "bg-[#5CBF92]" : "bg-[#DF3A3A]"}`}>
-      {status === "delivered" ? <CheckCircle className="text-white" /> : <AlertTriangle className="text-white" />}
+    <div className={`status ${status === true ? "bg-[#5CBF92]" : "bg-[#DF3A3A]"}`}>
+      {status === false ? <CheckCircle className="text-white" /> : <AlertTriangle className="text-white" />}
 
       <p className="text-white font-extralight">{status === "delivered" ? "Remis" : "Non remis"}</p>
     </div>
